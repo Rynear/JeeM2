c69046172de99e860ab603aae5c2ee661eaede1f
Julien <Julien@192.168.254.71>
2018-11-16 17:25:26 +0100

    Restart vueEleve 3

--- src/java/web/VueEleve.java
+++ src/java/web/VueEleve.java
@@ -5,9 +5,18 @@
  */
 package web;
 
+import dao.EleveFacadeLocal;
+import dao.ProfFacadeLocal;
+import entity.Eleve;
 import javax.inject.Named;
 import javax.enterprise.context.SessionScoped;
 import java.io.Serializable;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import org.primefaces.event.ToggleEvent;
 
 /**
  *
@@ -17,10 +26,150 @@ import java.io.Serializable;
 @SessionScoped
 public class VueEleve implements Serializable {
 
+    @EJB
+    EleveFacadeLocal eleveDAO;
+    @EJB
+    ProfFacadeLocal profDAO;
+    private Eleve monEleve;
+    private Integer idEleve;
+    private String nomEleve;
+    private String prenomEleve;
+    private Integer sexeEleve;
+    private Integer ageEleve;
+    private Integer niveauEleve;
+    private String mdpEleve;
+    private String message;
+    private String option;
+    
     /**
      * Creates a new instance of VueEleve
      */
     public VueEleve() {
     }
     
+    @PostConstruct
+    public void init () {
+        monEleve = new Eleve();
+    }
+    
+    public List<Eleve> getListeEleve() {
+        return listeEleve;
+    }
+
+    public void setListeEleve(List<Eleve> listeEleve) {
+        this.listeEleve = listeEleve;
+    }
+    private List<Eleve> listeEleve;
+
+    public EleveFacadeLocal getEleveDAO() {
+        return eleveDAO;
+    }
+
+    public void setEleveDAO(EleveFacadeLocal eleveDAO) {
+        this.eleveDAO = eleveDAO;
+    }
+
+    public ProfFacadeLocal getProfDAO() {
+        return profDAO;
+    }
+
+    public void setProfDAO(ProfFacadeLocal profDAO) {
+        this.profDAO = profDAO;
+    }
+
+    public Eleve getMonEleve() {
+        return monEleve;
+    }
+
+    public void setMonEleve(Eleve monEleve) {
+        this.monEleve = monEleve;
+    }
+
+    public Integer getIdEleve() {
+        return idEleve;
+    }
+
+    public void setIdEleve(Integer idEleve) {
+        this.idEleve = idEleve;
+    }
+
+    public String getNomEleve() {
+        return nomEleve;
+    }
+
+    public void setNomEleve(String nomEleve) {
+        this.nomEleve = nomEleve;
+    }
+
+    public String getPrenomEleve() {
+        return prenomEleve;
+    }
+
+    public void setPrenomEleve(String prenomEleve) {
+        this.prenomEleve = prenomEleve;
+    }
+
+    public Integer getSexeEleve() {
+        return sexeEleve;
+    }
+
+    public void setSexeEleve(Integer sexeEleve) {
+        this.sexeEleve = sexeEleve;
+    }
+
+    public Integer getAgeEleve() {
+        return ageEleve;
+    }
+
+    public void setAgeEleve(Integer ageEleve) {
+        this.ageEleve = ageEleve;
+    }
+
+    public Integer getNiveauEleve() {
+        return niveauEleve;
+    }
+
+    public void setNiveauEleve(Integer niveauEleve) {
+        this.niveauEleve = niveauEleve;
+    }
+
+    public String getMdpEleve() {
+        return mdpEleve;
+    }
+
+    public void setMdpEleve(String mdpEleve) {
+        this.mdpEleve = mdpEleve;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getOption() {
+        return option;
+    }
+
+    public void setOption(String option) {
+        this.option = option;
+    }
+
+    public void ajoutEleve(){
+        eleveDAO.create(monEleve);
+        saveMessage();
+    }
+        
+    public void saveMessage() {
+        FacesContext context = FacesContext.getCurrentInstance();
+         
+        context.addMessage(null, new FacesMessage("Successful") );
+    }
+
+    public void handleToggle(ToggleEvent event) {
+        FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "Toggled", "Visibility:" + event.getVisibility());
+        FacesContext.getCurrentInstance().addMessage(null, msg);
+    }
 }

81e0028e2af9fe07e1422da0945fa1acbba69be2
felix <felix.salles@arondor.com>
2018-11-18 22:34:31 +0100

    Ajout des méthodes/attributs d'eleve

--- src/java/web/VueEleve.java
+++ src/java/web/VueEleve.java
@@ -5,9 +5,12 @@
  */
 package web;
 
+import dao.EleveFacadeLocal;
+import entity.Eleve;
 import javax.inject.Named;
 import javax.enterprise.context.SessionScoped;
 import java.io.Serializable;
+import javax.ejb.EJB;
 
 /**
  *
@@ -15,12 +18,101 @@ import java.io.Serializable;
  */
 @Named(value = "vueEleve")
 @SessionScoped
-public class vueEleve implements Serializable {
+public class VueEleve implements Serializable {
 
+    @EJB
+    EleveFacadeLocal eleveDAO;
+    private Eleve monEleve;
+    private Eleve monNouveauEleve;
+    private Eleve monAncienEleve;
+    private String nomE;
+    private String prenomE;
+    private String mdpE;
+    private Integer sexeE;
+    private Integer niveauE;
+    
+            
     /**
      * Creates a new instance of vueEleve
      */
-    public vueEleve() {
+    public VueEleve() {
+        monNouveauEleve = new Eleve();
+    }
+
+    public EleveFacadeLocal getEleveDAO() {
+        return eleveDAO;
+    }
+
+    public void setEleveDAO(EleveFacadeLocal eleveDAO) {
+        this.eleveDAO = eleveDAO;
+    }
+
+    public Eleve getMonEleve() {
+        return monEleve;
+    }
+
+    public void setMonEleve(Eleve monEleve) {
+        this.monEleve = monEleve;
+    }
+
+    public Eleve getMonNouveauEleve() {
+        return monNouveauEleve;
+    }
+
+    public void setMonNouveauEleve(Eleve monNouveauEleve) {
+        this.monNouveauEleve = monNouveauEleve;
+    }
+
+    public Eleve getMonAncienEleve() {
+        return monAncienEleve;
+    }
+
+    public void setMonAncienEleve(Eleve monAncienEleve) {
+        this.monAncienEleve = monAncienEleve;
+    }
+
+    public String getNomE() {
+        return nomE;
+    }
+
+    public void setNomE(String nomE) {
+        this.nomE = nomE;
+    }
+
+    public String getPrenomE() {
+        return prenomE;
+    }
+
+    public void setPrenomE(String prenomE) {
+        this.prenomE = prenomE;
+    }
+
+    public String getMdpE() {
+        return mdpE;
+    }
+
+    public void setMdpE(String mdpE) {
+        this.mdpE = mdpE;
+    }
+
+    public Integer getSexeE() {
+        return sexeE;
+    }
+
+    public void setSexeE(Integer sexeE) {
+        this.sexeE = sexeE;
+    }
+
+    public Integer getNiveauE() {
+        return niveauE;
+    }
+
+    public void setNiveauE(Integer niveauE) {
+        this.niveauE = niveauE;
+    }
+    
+    public void addNewEleve() {
+        eleveDAO.create(monNouveauEleve);
     }
     
 }

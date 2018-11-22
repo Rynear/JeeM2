869c366960564dd577c4acdcc8e48dc85b1bd00e
felix <felix.salles@arondor.com>
2018-11-22 11:15:22 +0100

    authentification prof modif et proposition de cours

--- src/java/web/VueProf.java
+++ src/java/web/VueProf.java
@@ -5,12 +5,14 @@
  */
 package web;
 
+import dao.CoursFacadeLocal;
 import dao.ProfFacadeLocal;
 import entity.Cours;
 import entity.Prof;
 import javax.inject.Named;
 import javax.enterprise.context.SessionScoped;
 import java.io.Serializable;
+import java.util.Date;
 import java.util.List;
 import javax.ejb.EJB;
 import javax.faces.application.FacesMessage;
@@ -26,6 +28,9 @@ public class VueProf implements Serializable {
 
     @EJB
     ProfFacadeLocal profDAO;
+    @EJB
+    CoursFacadeLocal coursDAO;
+    private Cours newCours;
     private Prof monProf;
     private Prof monNouveauProf;
     private Prof monAncienProf;
@@ -35,13 +40,17 @@ public class VueProf implements Serializable {
     private String mdpP;
     private Integer sexeP;
     private Integer niveauMaxP;
+    private Integer recordIdProf;
     private Cours Cours1;
+    private Date dateNewCours;
+    private String newComment;
 
     /**
      * Creates a new instance of VueProf
      */
     public VueProf() {
         monNouveauProf = new Prof();
+        newCours = new Cours();
     }
 
     public ProfFacadeLocal getProfDAO() {
@@ -131,14 +140,92 @@ public class VueProf implements Serializable {
     public List<Prof> getListeProf() {
         return profDAO.findAll();
     }
+
+    public Integer getRecordIdProf() {
+        return recordIdProf;
+    }
+
+    public void setRecordIdProf(Integer recordIdProf) {
+        this.recordIdProf = recordIdProf;
+    }
+
+    public Date getDateNewCours() {
+        return dateNewCours;
+    }
+
+    public void setDateNewCours(Date dateNewCours) {
+        this.dateNewCours = dateNewCours;
+    }
+
+    public String getNewComment() {
+        return newComment;
+    }
+
+    public void setNewComment(String newComment) {
+        this.newComment = newComment;
+    }
     
-    public void editProf() {
-        monAncienProf = profDAO.find(IdP);
-        monAncienProf.setNomP(monNouveauProf.getNomP());
+    
+    
+    
+    
+    public String loginPropoCours() {
+        List<Prof> listProfLogin = profDAO.findAll();
+        FacesContext context = FacesContext.getCurrentInstance();
+        if (nomP.equals("") || prenomP.equals("") || mdpP.equals("")){
+            nomP="";
+            prenomP="";
+            mdpP="";
+            return null;
+        } else {
+            for (Prof prof : listProfLogin){
+                 if((prof.getNomP().equals(nomP)) && (prof.getPrenomP().equals(prenomP)) && (prof.getMdpP().equals(mdpP))){
+                     setRecordIdProf(prof.getIdP());
+                     context.addMessage(null, new FacesMessage("Authentificaiton réussi"));
+                     return "ProposerUnCours.xhtml?faces-redirect=true";
+                 }
+            }
+        context.addMessage(null, new FacesMessage("Mot de passe ou login invalide, veuillez réessayer"));    
+        return null;
+        }
+    }
+    
+    public String loginModifCours() {
+        List<Prof> listProfLogin = profDAO.findAll();
+        FacesContext context = FacesContext.getCurrentInstance();
+        if (nomP.equals("") || prenomP.equals("") || mdpP.equals("")){
+            nomP="";
+            prenomP="";
+            mdpP="";
+            return null;
+        } else {
+            for (Prof prof : listProfLogin){
+                 if((prof.getNomP().equals(nomP)) && (prof.getPrenomP().equals(prenomP)) && (prof.getMdpP().equals(mdpP))){
+                     setRecordIdProf(prof.getIdP());
+                     context.addMessage(null, new FacesMessage("Authentificaiton réussi"));
+                     return "ModifProf.xhtml?faces-redirect=true";
+                 }
+            }
+        context.addMessage(null, new FacesMessage("Mot de passe ou login invalide, veuillez réessayer"));    
+        return null;
+        }
+    }
+    
+    public String createNewCours(){
+        Prof pf = profDAO.find(getRecordIdProf());        
+        newCours.setIdP(pf);
+        newCours.setDateHeureC(dateNewCours);
+        newCours.setCommentaireC(newComment);
+        coursDAO.create(newCours);
+        return "index.xhtml?faces-redirect=true";
+    }
+    
+    public String editProf() {
+        monAncienProf = profDAO.find(getRecordIdProf());
         monAncienProf.setNiveauMaxP(monNouveauProf.getNiveauMaxP());
         profDAO.edit(monAncienProf);
         FacesContext context = FacesContext.getCurrentInstance();
         context.addMessage(null, new FacesMessage("Successful !! Modifications réalisés avec succès.  "));
+        return "index.xhtml?faces-redirect=true";
     }
-
 }

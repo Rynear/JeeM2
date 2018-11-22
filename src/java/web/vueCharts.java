ebf82d145cf5fd947f6bfeb6c59e5d43ff89cc01
Julien <Julien@192.168.254.71>
2018-11-22 09:26:51 +0100

    Merge origin/master
    Conflicts:
    	src/java/web/VueCours.java

--- src/java/web/vueCharts.java
+++ src/java/web/vueCharts.java
@@ -14,8 +14,13 @@ import dao.ProfFacadeLocal;
 import entity.Prof;
 import dao.EleveFacadeLocal;
 import entity.Eleve;
+import java.util.ArrayList;
 import java.util.List;
+import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import org.primefaces.event.ItemSelectEvent;
 import org.primefaces.model.chart.PieChartModel;
 
 /**
@@ -39,7 +44,52 @@ public class vueCharts implements Serializable {
     private List<Prof> listProf;
     private List<Eleve> listEleve;
     private PieChartModel model;
+    private PieChartModel pieModel1;
+    private PieChartModel livePieModel;
+    private PieChartModel livePModel;
 
+    public void setListCours(List<Cours> listCours) {
+        this.listCours = listCours;
+    }
+
+    public void setListProf(List<Prof> listProf) {
+        this.listProf = listProf;
+    }
+
+    public void setPieModel1(PieChartModel pieModel1) {
+        this.pieModel1 = pieModel1;
+    }
+
+    public void setLivePModel(PieChartModel livePModel) {
+        this.livePModel = livePModel;
+    }
+
+    public void setLivePieModel(PieChartModel livePieModel) {
+        this.livePieModel = livePieModel;
+    }
+
+    @PostConstruct
+    public void init() {
+        createPieModels();
+        listEleve = new ArrayList<Eleve>();
+        listEleve = eleveDAO.findAll();
+        listProf = new ArrayList<Prof>();
+        listProf = profDAO.findAll();
+        listCours = new ArrayList<Cours>();
+        listCours = coursDAO.findAll();
+    }
+    
+    public void itemSelect(ItemSelectEvent event) {
+        FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "Item selected",
+                "Item Index: " + event.getItemIndex() + ", Series Index:" + event.getSeriesIndex());
+ 
+        FacesContext.getCurrentInstance().addMessage(null, msg);
+    }
+    
+    public PieChartModel getPieModel1() {
+        return pieModel1;
+    }
+    
     public PieChartModel getModel() {
         return model;
     }
@@ -76,9 +126,9 @@ public class vueCharts implements Serializable {
         return profDAO.findAll();
     }
 
-    public Integer sizeListProf() {
-        return listProf.size();
-    }
+//    public Integer sizeListProf() {
+//        return listProf.size();
+//    }
 
     public List<Eleve> getListEleve() {
         return eleveDAO.findAll();
@@ -116,14 +166,41 @@ public class vueCharts implements Serializable {
         return coursDAO.findAll();
     }
 
-
-    public void graph(){
-        model = new PieChartModel();
-        model.set("Nb cours", 12);
-        model.set("Nb Profs", 48);
-        model.set("Nb Eleve",12 );
-        model.setTitle("Simple Pie");
-        model.setLegendPosition("w");
+    private void createPieModels() {
+        createPieModel1();
+    }
+    
+    private void createPieModel1() {
+        pieModel1 = new PieChartModel();
+ 
+        pieModel1.set("Nombre de profs", 250);
+        pieModel1.set("Nombre d'élèves", 325);
+        pieModel1.set("Nombe de cours", 702);
+ 
+        pieModel1.setTitle("Découvrez nos chiffres impressionnants !!!");
+        pieModel1.setLegendPosition("w");
+        pieModel1.setShadow(true);
+    }
+    
+    public PieChartModel getLivePModel() {
+        PieChartModel livePModel = new PieChartModel();
+        
+        listEleve = eleveDAO.findAll();
+        int tailleEleve = listEleve.size();
+        listProf = profDAO.findAll();
+        int tailleProf = listProf.size();
+        listCours = coursDAO.findAll();
+        int tailleCours = listCours.size();
+        
+        livePModel.getData().put("Nombre d'élèves inscrits", tailleEleve);
+        livePModel.getData().put("Nombre de professeurs inscrits", tailleProf);
+        livePModel.getData().put("Nombre de cours dispensés", tailleCours);
+ 
+        livePModel.setTitle("Les chiffres du site");
+        livePModel.setLegendPosition("w");
+        
+        setLivePieModel(livePModel);
+        return livePieModel;
     }
     /**
      * Creates a new instance of vueCharts
